206a21f0a4f76a976767426a82c1c473848ed09c
Jorge Apaza <jorgeapaza2124@gmail.com>
2022-06-16 16:49:59 -0300

    cambios de js

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 //lista de productos
+class Producto{
+    constructor(nombre, precio) {
+        this.nombre  = nombre.toUpperCase();
+        this.precio  = precio.parseFloat(precio);
+        this.vendido = false;
+    }
+    
+    sumaIva()  {
+        this.precio = this.precio * 1,21;
+    }
+    vender() {
+        this.vendido = true;
+    }
+}
 
-const listaNombres = [];
-let cantidad = 10;
-//Empleo de do...while para cargar nombres en el array por prompt()
-do {
-    let entrada = prompt ("Ingresar nombres");
-    listaNombres.push(entrada.toUpperCase());
-    console.log(listaNombres.length);
-} while (listaNombres.length != cantidad);
-//concatenamos un mismo array de dos elementos
-const nuevaLista = listaNombres.concat(["proteina" , "aminiaciso"]);
-//salida con salto de linea join
-alert(nuevaLista.join("\n"));
+
+const productos = [];
+productos.push(new Productos("Mancuernas", "6500"));
+productos.push(new Productos("PesasRusas", "9000"));
+productos.push(new Productos("Guantes", "3500"));
+productos.push(new Productos("ConjuntoDepontivoH", "10000"));
+productos.push(new Productos("ConjuntoDepontivoM", "12500"));
+productos.push(new Productos("BotellaDeAgua", "4500"));
+
+  for(const producto of productos)
+  producto.sumaIva();
+  producto.vender();
+
+  console.log(productos.producto)
+  productos.includes(productos.Productos)
+
+  let filtrado = productos.filter((el) => el.nombre.includes("ConjuntoDepontivo"))
+  console.log(filtrado);
