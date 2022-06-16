38238a479b6e87ad62d870e57e3f768d0f2bfbf4
Jorge Apaza <jorgeapaza2124@gmail.com>
2022-06-16 19:18:56 -0300

    cambios en el carrito

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 class Producto{
     constructor(nombre, precio) {
         this.nombre  = nombre.toUpperCase();
-        this.precio  = precio.parseFloat(precio);
+        this.precio  = parseFloat(precio);
         this.vendido = false;
     }
     
     sumaIva()  {
-        this.precio = this.precio * 1,21;
+        this.precio = this.precio * 1.21;
     }
     vender() {
         this.vendido = true;
@@ -16,19 +16,31 @@ class Producto{
 
 
 const productos = [];
-productos.push(new Productos("Mancuernas", "6500"));
-productos.push(new Productos("PesasRusas", "9000"));
-productos.push(new Productos("Guantes", "3500"));
-productos.push(new Productos("ConjuntoDepontivoH", "10000"));
-productos.push(new Productos("ConjuntoDepontivoM", "12500"));
-productos.push(new Productos("BotellaDeAgua", "4500"));
+productos.push(new Producto("Mancuernas", 6500));
+productos.push(new Producto("PesasRusas", 9000));
+productos.push(new Producto("Guantes", 3500));
+productos.push(new Producto("ConjuntoDepontivoH", 10000));
+productos.push(new Producto("ConjuntoDepontivoM", 12500));
+productos.push(new Producto("BotellaDeAgua", 4500));
 
-  for(const producto of productos)
-  producto.sumaIva();
-  producto.vender();
+  for(const producto of productos){
+    producto.sumaIva();
+    producto.vender();
+  }
+  
 
-  console.log(productos.producto)
-  productos.includes(productos.Productos)
+  console.log(productos);
+  const item = prompt(); 
 
-  let filtrado = productos.filter((el) => el.nombre.includes("ConjuntoDepontivo"))
-  console.log(filtrado);
+ // productos.includes(productos.Productos)
+
+  let filtrado = productos.filter((el) => el.nombre.includes(item.toUpperCase()));
+  if (filtrado){
+    console.log("se encontro el producto");
+    console.log(filtrado);
+  } else {
+    console.log("no se encontro el producto");
+  }
+
+
+  
